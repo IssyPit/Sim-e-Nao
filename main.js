fed952eb1ee8a8c74d393998b2ab4ccacdfd24f3
Issya <122482111+IssyPit@users.noreply.github.com>
2024-07-30 18:35:29 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+let botao1 = document.querySelector('#respostaS');
+
+botao1.addEventListener('click', ()=>{
+
+    alert('Você é alguém muito inteligente!!')
+})
+
+let botao2 = document.querySelector('#respostaN');
+
+botao2.addEventListener('click', ()=>{
+    
+    alert('Ai é sem graça...')
+})
